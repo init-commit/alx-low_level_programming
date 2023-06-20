25cefd94104ade24ca18d787fbe990db8e2d668f
Ian Mike <iammikeian@gmail.com>
2023-06-20 21:48:17 +0300

    Prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that prints the alphabetical order
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}
